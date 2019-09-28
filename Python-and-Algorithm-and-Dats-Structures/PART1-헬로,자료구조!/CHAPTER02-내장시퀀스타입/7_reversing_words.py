5b1f0361a1135842ad03e8984eb01b59d87e0d4a
joeunyeob <naramharam@naver.com>
2019-09-28 20:04:36 +0900

    chapter02 update

---
+++ Python-and-Algorithm-and-Dats-Structures/PART1-헬로,자료구조!/CHAPTER02-내장시퀀스타입/7_reversing_words.py
@@ -0,0 +1,18 @@
+def reversing_words(str1):
+    words = str1.split(" ")
+    rev_set = " ".join(reversed(words))
+    return rev_set
+
+
+def reversing_words2(str1):
+    words = str1.split(" ")
+    words.reverse()
+    return " ".join(words)
+
+
+if __name__ == '__main__':
+    str1 = '파이썬 알고르짐 재미있다.'
+    str2 = reversing_words(str1)
+    str3 = reversing_words(str1)
+    print(str2)
+    print(str3)

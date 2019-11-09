8f025738c38b4b9a07117ac14fcc61aa06a3cfdd
joeunyeob <naramharam@naver.com>
2019-11-09 21:46:12 +0900

    chapter06 update

--- Python-and-Algorithm-and-Dats-Structures/Python-and-Algorithm-and-Dats-Structures/PART1-헬로,자료구조!/CHAPTER05-객체지향 설계/8_observer_pattern_with_event.py
+++ Python-and-Algorithm-and-Dats-Structures/Python-and-Algorithm-and-Dats-Structures/PART1-헬로,자료구조!/CHAPTER05-객체지향 설계/8_observer_pattern_with_event.py
@@ -1,7 +1,14 @@
-class Publisher(object):
+class Subscriber(object):
+    def __init__(self, name):
+        self.name = name
 
+    def update(self, message):
+        print('{0}, {1}'.format(self.name, message))
+
+
+class Publisher(object):
     def __init__(self, events):
-        self.subscribers = {events:dict() for event in events}
+        self.subscribers = {events: dict() for event in events}
 
     def get_subscribers(self, event):
         return self.subscribers[event]
@@ -14,3 +21,19 @@ class Publisher(object):
     def dispatch(self, event, message):
         for subscriber, callback in self.get_subscribers(event).items():
             callback(message)
+
+
+if __name__ == '__main__':
+    pub = Publisher(['점심', '퇴근'])
+
+    astin = Subscriber('아스틴')
+    james = Subscriber('제임스')
+    jeff = Subscriber('제프')
+
+    pub.register('점심', astin)
+    pub.register('퇴근', astin)
+    pub.register('퇴근', james)
+    pub.register('점심', jeff)
+
+    pub.dispatch('점심', '점심시간입니다.')
+    pub.dispatch('퇴근', '저녁시간입니다.')
